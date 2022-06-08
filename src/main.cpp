3769c5c735dfee8a77d8ebd8541e2f93feac8c14
sami-hatna66 <sami.hatna2003@gmail.com>
2022-06-08 16:26:55 +0100

    SDL skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,49 @@
 #include <iostream>
 using namespace std;
 
+SDL_Window* win = NULL;
+SDL_Surface* surface = NULL;
+SDL_Renderer* render = NULL;
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void init() {
+    SDL_Init(SDL_INIT_VIDEO);
+    win = SDL_CreateWindow("SYCL Crowd Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void draw() {
+    SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+    SDL_RenderClear(render);
+
+    SDL_RenderPresent(render);
+}
+
+void close() {
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
+
 int main() {
-    cout << "Hello World!!";
+    init();
+
+    draw();
+
+    bool isQuit = false;
+    SDL_Event event;
+
+    while(!isQuit) {
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isQuit = true;
+            }
+        }
+
+        draw();
+    }
+
+    close();
     return 0;
 }
