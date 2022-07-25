947c8f0103a3db0f5fb1a02e8e306899652489f3
sami-hatna66 <sami.hatna2003@gmail.com>
2022-07-25 15:25:40 +0100

    Graph plotting error handling

--- scripts/PlotGraphs.py
+++ scripts/PlotGraphs.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
+import os.path
 from threading import Thread
 import time
 
@@ -16,50 +17,64 @@ animationThread = Thread(target=workingAnimation)
 animationThread.start()
 
 # Plot average forces
-with open("../output/csv/averageForces.csv") as file:
-    reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
-    averageForces = list(reader)
-    averageForces = list(zip(*averageForces))
+if not os.path.exists("../output/csv/averageForces.csv"):
+    print("Error: couldn't find averageForces.csv")
+else:
+    with open("../output/csv/averageForces.csv") as file:
+        reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
+        averageForces = list(reader)
+        averageForces = list(zip(*averageForces))
 
-plt.plot(averageForces[0], averageForces[1])
-plt.xlabel("Timestep")
-plt.ylabel("Average Force (N)")
-plt.title("Average Force Applied to Actor")
-plt.savefig("../output/images/averageForcesGraph.png")
+    plt.plot(averageForces[0], averageForces[1])
+    plt.xlabel("Timestep")
+    plt.ylabel("Average Force (N)")
+    plt.title("Average Force Applied to Actor")
+    plt.savefig("../output/images/averageForcesGraph.png")
 
-plt.clf()
+    plt.clf()
 
-with open("../output/csv/kernelDurations.csv") as file:
-    reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
-    kernelDurations = list(reader)
-    kernelDurations = list(zip(*kernelDurations))
+# Plot kernel execution times
+if not os.path.exists("../output/csv/kernelDurations.csv"):
+    print("Error: couldn't find kernelDurations.csv")
+else:
+    with open("../output/csv/kernelDurations.csv") as file:
+        reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
+        kernelDurations = list(reader)
+        kernelDurations = list(zip(*kernelDurations))
 
-plt.plot(kernelDurations[0], kernelDurations[1], linewidth=0.5)
-plt.xlabel("Kernel Number")
-plt.ylabel("Execution Time (μs)")
-plt.title("Kernel Execution Times")
-plt.savefig("../output/images/kernelDurationsGraph.png")
+    plt.plot(kernelDurations[0], kernelDurations[1], linewidth=0.5)
+    plt.xlabel("Kernel Number")
+    plt.ylabel("Execution Time (μs)")
+    plt.title("Kernel Execution Times")
+    plt.savefig("../output/images/kernelDurationsGraph.png")
 
-plt.clf()
+    plt.clf()
 
-with open("../output/csv/destinationTimes.csv") as file:
-    reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
-    destinationTimes = []
-    for entry in reader:
-        if entry[1] != -1:
-           destinationTimes.append([int(entry[0]), int(entry[1])]) 
-    destinationTimes = list(zip(*destinationTimes))
+# Plot actor arrival times
+if not os.path.exists("../output/csv/destinationTimes.csv"):
+    print("Error: couldn't find distinationTimes.csv")
+else:
+    with open("../output/csv/destinationTimes.csv") as file:
+        reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
+        destinationTimes = []
+        for entry in reader:
+            if entry[1] != -1:
+                destinationTimes.append([int(entry[0]), int(entry[1])]) 
+        destinationTimes = list(zip(*destinationTimes))
 
-    operatingOn = sorted(destinationTimes[1])
+        operatingOn = sorted(destinationTimes[1])
 
-    alteredDestinationTimes = [[i for i in range(0, max(operatingOn))], [0 for i in range(0, max(operatingOn))]]
-    for timestamp in operatingOn:
-        if timestamp != -1:
-            for x in range(timestamp, max(operatingOn)):
-                alteredDestinationTimes[1][x] += 1
+        alteredDestinationTimes = [[i for i in range(0, max(operatingOn))], [0 for i in range(0, max(operatingOn))]]
+        for timestamp in operatingOn:
+            if timestamp != -1:
+                for x in range(timestamp, max(operatingOn)):
+                    alteredDestinationTimes[1][x] += 1
 
-if len(destinationTimes) > 0:
-    plt.plot(alteredDestinationTimes[0], alteredDestinationTimes[1])
-    plt.savefig("../output/images/destinationTimesGraph.png")
+    if len(destinationTimes) > 0:
+        plt.plot(alteredDestinationTimes[0], alteredDestinationTimes[1])
+        plt.xlabel("No. of Actors at Destination")
+        plt.ylabel("Timestep")
+        plt.title("Actor Arrival Times at Destina")
+        plt.savefig("../output/images/destinationTimesGraph.png")
 
 complete = True
